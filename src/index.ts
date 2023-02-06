072792698841597b0248ad99c1074ab4719ddcc9
Philihp Busby <philihp@gmail.com>
2023-02-06 09:34:23 +0000

    fix: defer to pcg

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,9 @@
-// // PCG-XSH-RR
-const mask = 0xffffffffffffffffn
-const increment = 1442695040888963407n
-const multiplier = 63641362238467932305n
+import { createPcg32, randomInt } from 'pcg'
 
-const rotr32 = (x: bigint, r: bigint) => (x >> r) | (x << (-r & 31n))
+export type PCGState = number
 
-export type PCGState = bigint
+export const newRandGen = (seed: number, stream = 1): PCGState => createPcg32({}, seed, stream)
 
-export const newRandGen = (seed: number): PCGState => {
-  return BigInt.asUintN(64, BigInt(seed) + increment)
-}
+export const randNext = (state: PCGState): [number, PCGState] => randomInt(0, 2 ** 32 - 1, state)
 
-export const randNext = (state: PCGState): [number, PCGState] => [
-  Number(BigInt.asUintN(32, rotr32((state ^ (state >> 18n)) >> 27n, state >> 59n))),
-  (state * multiplier + increment) & mask,
-]
-
-export const randRange = (min: number, max: number, state: PCGState): [number, PCGState] => {
-  const [n, newState] = randNext(state)
-  const range = max - min
-  return [(n % range) + min, newState]
-}
+export const randRange = (min: number, max: number, state: PCGState): [number, PCGState] => randomInt(min, max, state)
